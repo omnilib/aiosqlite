11466e129c652e87d1cb404b935df427c6322f07
John Reese <john@noswap.com>
2018-05-04 20:29:44 -0700

    blacken setup

--- setup.py
+++ setup.py
@@ -1,36 +1,35 @@
 from setuptools import setup
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('aiosqlite/__init__.py') as f:
+with open("aiosqlite/__init__.py") as f:
     for line in f:
-        if line.startswith('__version__'):
+        if line.startswith("__version__"):
             version = line.split('"')[1]
 
 setup(
-    name='aiosqlite',
-    description='asyncio bridge to the standard sqlite3 module',
+    name="aiosqlite",
+    description="asyncio bridge to the standard sqlite3 module",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     version=version,
-    author='John Reese',
-    author_email='john@noswap.com',
-    url='https://github.com/jreese/aiosqlite',
+    author="John Reese",
+    author_email="john@noswap.com",
+    url="https://github.com/jreese/aiosqlite",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: AsyncIO',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries',
+        "Development Status :: 4 - Beta",
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries",
     ],
-    license='MIT',
-    packages=['aiosqlite'],
-    setup_requires=['setuptools>=38.6.0'],
-    install_requires=[
-    ],
+    license="MIT",
+    packages=["aiosqlite"],
+    setup_requires=["setuptools>=38.6.0"],
+    install_requires=[],
 )
