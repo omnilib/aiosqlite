299d89ff7930c0c641d40ed449b78f41b256bb7f
John Reese <john@noswap.com>
2018-09-03 11:55:30 -0700

    Version bump v0.4.0

--- aiosqlite/__init__.py
+++ aiosqlite/__init__.py
@@ -7,7 +7,7 @@ from sqlite3 import sqlite_version, sqlite_version_info
 
 from .core import connect, Connection, Cursor
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __all__ = [
     "__version__",
     "sqlite_version",
