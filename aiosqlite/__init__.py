9926272d98173f784701ad34b33417e247a9008b
John Reese <john@noswap.com>
2018-09-09 14:28:57 -0700

    Add more aliases to the sqlite3 module from __init__
    This should allow for fewer cases of needing to import both
    aiosqlite and sqlite3 in pursuit of advanced use cases.

--- aiosqlite/__init__.py
+++ aiosqlite/__init__.py
@@ -3,16 +3,39 @@
 
 """asyncio bridge to sqlite3"""
 
-from sqlite3 import sqlite_version, sqlite_version_info
+from sqlite3 import (  # pylint: disable=redefined-builtin
+    register_adapter,
+    register_converter,
+    sqlite_version,
+    sqlite_version_info,
+    Row,
+    Warning,
+    Error,
+    DatabaseError,
+    IntegrityError,
+    ProgrammingError,
+    OperationalError,
+    NotSupportedError,
+)
 
 from .core import connect, Connection, Cursor
 
 __version__ = "0.4.0"
 __all__ = [
     "__version__",
+    "register_adapter",
+    "register_converter",
     "sqlite_version",
     "sqlite_version_info",
     "connect",
     "Connection",
     "Cursor",
+    "Row",
+    "Warning",
+    "Error",
+    "DatabaseError",
+    "IntegrityError",
+    "ProgrammingError",
+    "OperationalError",
+    "NotSupportedError",
 ]
