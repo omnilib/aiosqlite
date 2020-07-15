7a83b7c816113504a2e1ab848349ce0c30a777c6
John Reese <john@noswap.com>
2020-07-14 23:09:08 -0700

    Fix type annotations and base classes for @contextmanager results

--- aiosqlite/context.py
+++ aiosqlite/context.py
@@ -2,26 +2,27 @@
 # Licensed under the MIT license
 
 
-import collections.abc
 from functools import wraps
-from typing import Any, Callable, Coroutine, TypeVar
+from typing import Any, Callable, Coroutine, Generator, TypeVar
 
 from typing_extensions import AsyncContextManager
+
+from .cursor import Cursor
 
 _T = TypeVar("_T")
 
 
-class ContextManager(collections.abc.Coroutine):
+class Result(AsyncContextManager[_T], Coroutine[Any, Any, _T]):
     __slots__ = ("_coro", "_obj")
 
-    def __init__(self, coro):
+    def __init__(self, coro: Coroutine[Any, Any, _T]):
         self._coro = coro
-        self._obj = None
+        self._obj: _T
 
-    def send(self, value):
+    def send(self, value) -> None:
         return self._coro.send(value)
 
-    def throw(self, typ, val=None, tb=None):
+    def throw(self, typ, val=None, tb=None) -> None:
         if val is None:
             return self._coro.throw(typ)
 
@@ -30,26 +31,26 @@ class ContextManager(collections.abc.Coroutine):
 
         return self._coro.throw(typ, val, tb)
 
-    def close(self):
+    def close(self) -> None:
         return self._coro.close()
 
-    def __await__(self):
+    def __await__(self) -> Generator[Any, None, _T]:
         return self._coro.__await__()
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> _T:
         self._obj = await self._coro
         return self._obj
 
-    async def __aexit__(self, exc_type, exc, tb):
-        await self._obj.close()
-        self._obj = None
+    async def __aexit__(self, exc_type, exc, tb) -> None:
+        if isinstance(self._obj, Cursor):
+            await self._obj.close()
 
 
 def contextmanager(
     method: Callable[..., Coroutine[Any, Any, _T]]
-) -> Callable[..., AsyncContextManager[_T]]:
+) -> Callable[..., Result[_T]]:
     @wraps(method)
-    def wrapper(self, *args, **kwargs) -> ContextManager:
-        return ContextManager(method(self, *args, **kwargs))
+    def wrapper(self, *args, **kwargs) -> Result[_T]:
+        return Result(method(self, *args, **kwargs))
 
     return wrapper
