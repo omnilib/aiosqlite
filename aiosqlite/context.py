a157e0b7d126daeeda4e5b7dcf2b6cb9cf9ac274
John Reese <john@noswap.com>
2021-05-16 14:18:18 -0700

    Drop dependency on typing_extensions, fix #114

--- aiosqlite/context.py
+++ aiosqlite/context.py
@@ -3,9 +3,7 @@
 
 
 from functools import wraps
-from typing import Any, Callable, Coroutine, Generator, TypeVar
-
-from typing_extensions import AsyncContextManager
+from typing import Any, AsyncContextManager, Callable, Coroutine, Generator, TypeVar
 
 from .cursor import Cursor
 
