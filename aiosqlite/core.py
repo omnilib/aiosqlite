898165153b07b97c50be9b4eb50e90c84b0f7782
J. Nick Koston <nick@koston.org>
2024-01-05 21:23:57 -1000

    lint

--- aiosqlite/core.py
+++ aiosqlite/core.py
@@ -12,8 +12,18 @@ from functools import partial
 from pathlib import Path
 from queue import Empty, Queue, SimpleQueue
 from threading import Thread
-from typing import (Any, AsyncIterator, Callable, Generator, Iterable, Literal,
-                    Optional, Tuple, Type, Union)
+from typing import (
+    Any,
+    AsyncIterator,
+    Callable,
+    Generator,
+    Iterable,
+    Literal,
+    Optional,
+    Tuple,
+    Type,
+    Union,
+)
 from warnings import warn
 
 from .context import contextmanager
